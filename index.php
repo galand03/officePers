03622e9906df22a65ec02b3c144bdfa3623890ee
galand03 <gal.and85@hotmail.com>
2017-10-29 11:29:58 -0300

    dsd
    dsds

---
+++ index.php
@@ -0,0 +1,384 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Gabriela Medina</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link rel="stylesheet" href="css/bootstrap.css" type="text/css">
+
+    <!-- Custom Fonts -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.css" type="text/css">
+
+    <!-- Plugin CSS -->
+    <link rel="stylesheet" href="css/animate.min.css" type="text/css">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="css/creative.css" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body id="page-top">
+
+    <nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand page-scroll" href="#page-top">Gabriela Medina</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a class="page-scroll" href="#page-top">Inicio</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#about">Nosotros</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#services">&Aacute;reas de Pr&aacute;ctica</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#portfolio">Clientes</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#contact">Contacto</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+
+    <header>
+        <div class="header-content">
+            <div class="header-content-inner">
+                <h1>Gabriela Medina</h1>
+                <h1>Especialista en Derecho de Seguros</h1>
+                <hr>
+                <p>Contamos con un equipo de trabajo multidisciplinario de primer nivel y gran experiencia profesional, dirigido por especialistas del rubro. La formación y el desarrollo constante de nuestros abogados es una de nuestras prioridades, por lo que muchos de ellos cuentan con estudios de posgrado...</p>
+                <a href="#contact" class="btn btn-primary btn-xl page-scroll">Contactenos</a>
+            </div>
+        </div>
+    </header>
+
+    <section class="bg-primary" id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                    <h2 class="section-heading">"La abogacía es una profesión que debería ser considerada noble y eterna como la idea de Justicia."</h2>
+                    <hr class="light">
+                    <p class="text-faded">
+                        Nuestro compromiso con la defensa de los trabajadores es nuestro mayor interés.<br>
+                        Nos especializamos en despidos, trabajo en negro, indemnizaciones,  accidentes de trabajo y acoso laboral.<br>
+                        También en reclamos de trabajadores de empresas de servicios eventuales o de aquellas empresas que tercerizan sus servicios (ej. Consultoras, empresas de mantenimiento, etc.).<br>
+                        Como abogados laborales nos mantenemos altamente capacitados para brindarle al trabajador una solución rápida y eficaz.<br>
+                        Asistimos al trabajador en el intercambio telegráfico, en la instancia administrativa del SECLO y en la instancia judicial.
+
+</p>
+                    <!--a href="#" class="btn btn-default btn-xl">Get Started!</a-->
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="services">
+        <div class="container">
+            <!--div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="section-heading">At Your Service</h2>
+                    <hr class="primary">
+                </div>
+            </div--
+            >
+                    <div class="container">
+            <!--div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="section-heading">At Your Service</h2>
+                    <hr class="primary">
+                </div>
+            </div-->
+            <div id="about" class="about">
+              <div class="row">
+              <div class="col-sm-7 wowload fadeInLeft animated" style="visibility: visible;">
+                <img src="img/law.jpg" style="width:600px;">
+              </div>
+              <div class="col-sm-5 wowload fadeInRight animated" style="visibility: visible;">
+              <div class="spacer"><h2 style="margin-top: 0px !important;"><!--i class="fa fa-flask"></i--><img src="img/comillas.png" style="opacity:0.8" /> Derecho Laboral</h2>
+              <p>El estudio cuenta con una sólida fuerza de trabajo constituida por abogados altamente capacitados y personal administrativo idóneo<br>
+                        CURRICULUM DE GABRIELA MEDINA<br>
+                        Abogado egresado de la Universidad Nacional de Buenos Aires<br>
+                        Posgrado...<br>
+						TESINA<br>
+						“LA CELEBRACION DEL CONTRATO DE SEGURO”<br>
+						ACTIVIDAD PROFESIONAL <br>
+						Ejerce su profesión activamente desde su graduación desempeñándose, principalmente, en el asesoramiento jurídico de empresas.</p>
+              <div class="social">
+              
+              <a href="#"><i class="fa fa-facebook fa-2x"></i></a> <a href="#"><i class="fa fa-dribbble fa-2x"></i></a> <a href="#"><i class="fa fa-twitter fa-2x"></i></a> <a href="#"><i class="fa fa-linkedin fa-2x"></i></a></div>
+              </div>
+              </div>
+              </div>  
+            </div>
+        </div>
+        </div>
+        <div class="container">
+            <!--div class="row">
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-4x fa-diamond wow bounceIn text-primary"></i>
+                        <h3>Sturdy Templates</h3>
+                        <p class="text-muted">Our templates are updated regularly so they don't break.</p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-4x fa-paper-plane wow bounceIn text-primary" data-wow-delay=".1s"></i>
+                        <h3>Ready to Ship</h3>
+                        <p class="text-muted">You can use this theme as is, or you can make changes!</p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-4x fa-newspaper-o wow bounceIn text-primary" data-wow-delay=".2s"></i>
+                        <h3>Up to Date</h3>
+                        <p class="text-muted">We update dependencies to keep things fresh.</p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-4x fa-heart wow bounceIn text-primary" data-wow-delay=".3s"></i>
+                        <h3>Made with Love</h3>
+                        <p class="text-muted">You have to make your websites with love these days!</p>
+                    </div>
+                </div>
+            </div-->
+        </div>
+    </section>
+
+    <section class="no-padding" id="portfolio">
+        <div class="container">
+            <!--div class="row no-gutter">
+                <div class="col-lg-4 col-sm-6">
+                    <a href="#" class="portfolio-box">
+                        <img src="img/portfolio/1.jpg" class="img-responsive" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    Category
+                                </div>
+                                <div class="project-name">
+                                    Project Name
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <a href="#" class="portfolio-box">
+                        <img src="img/portfolio/2.jpg" class="img-responsive" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    Category
+                                </div>
+                                <div class="project-name">
+                                    Project Name
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <a href="#" class="portfolio-box">
+                        <img src="img/portfolio/3.jpg" class="img-responsive" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    Category
+                                </div>
+                                <div class="project-name">
+                                    Project Name
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <a href="#" class="portfolio-box">
+                        <img src="img/portfolio/4.jpg" class="img-responsive" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    Category
+                                </div>
+                                <div class="project-name">
+                                    Project Name
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <a href="#" class="portfolio-box">
+                        <img src="img/portfolio/5.jpg" class="img-responsive" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    Category
+                                </div>
+                                <div class="project-name">
+                                    Project Name
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <a href="#" class="portfolio-box">
+                        <img src="img/portfolio/6.jpg" class="img-responsive" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    Category
+                                </div>
+                                <div class="project-name">
+                                    Project Name
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div-->
+            <div id="works" class="works">
+              <div class="row">
+             
+              <div class="col-guia wowload fadeInLeft animated" style="visibility: visible;">
+              <div class="spacer"><h2><img src="img/guia.png" style="opacity:0.8" /> El Derecho de Seguros Privados es una rama del Derecho Mercantil que actualmente tiene mucho auge en la práctica, ya que existen muchas Instituciones de Seguros que ofrecen diversos tipos de pólizas y coberturas: Vida, gastos médicos mayores, automóvil, agropecuarios, responsabilidad civil, entre otros.
+​
+Sin embargo, también son pocos los despachos de abogados que conocen esta rama del derecho y que no saben formular una demanda de este tipo correctamente, por lo que en la mayoría de los casos los aventurados no consiguen el éxito esperado, pues, el conocimiento y lenguaje técnico de ésta materia es confusa incluso para el profesional del derecho.      
+​
+Por tal razón, Gabriela ofrece y presta un servicio de excelencia y calidad para demandar a cualquier aseguradora por negativa de pago del contrato de seguro, ya que durante varios años hemos venido litigando esta materia y conocemos las leyes especiales que la regulan, así como las instancias correspondientes.</h2>
+              <br>
+
+<div id="u991"><span style="z-index: 123;
+width: 10px;
+text-align: center;
+font-size: 24px;
+position: relative;
+left: 12px;
+margin-top: 5px;
+margin-right: -10000px;
+min-height: 30px;">1</span></div>
+<p style="margin-left: 50px;">
+Consulte con su Abogado de confianza antes de efectuar cualquier tipo de reclamo. El desconocimiento  de sus derechos o de la Ley, puede llevarle a reclamar menos de lo que le corresponde, lo mismo que  exigir lo que no le es debido, lo que (en cualquiera de los casos) le ocasionará futuros inconvenientes.
+
+</p>
+<div id="u991"><span style="z-index: 123;
+width: 10px;
+text-align: center;
+font-size: 24px;
+position: relative;
+left: 12px;
+margin-top: 5px;
+margin-right: -10000px;
+min-height: 30px;">2</span></div>
+<p style="margin-left: 50px;">
+
+Tenga muy presente que cualquier notificación o reclamo dirigido a su empleador debe ser efectuado por medios "fehacientes". Debe quedar constancia de ello en algún registro que pueda ser consultado en el futuro y que permita probar que la notificación fue recibida, así como el contenido de la misma y la fecha. Por ello, no sirven los testigos, los llamados telefónicos ni los faxes.
+
+</p>
+<div id="u991"><span style="z-index: 123;
+width: 10px;
+text-align: center;
+font-size: 24px;
+position: relative;
+left: 12px;
+margin-top: 5px;
+margin-right: -10000px;
+min-height: 30px;">3</span></div>
+<p style="margin-left: 50px;">
+
+A los fines de iniciar un reclamo necesitará la documentación pertinente, que dependerá de cada caso, y de lo que sea necesario notificar y/o reclamar. Siempre, y como mínimo, serán imprescindibles los últimos recibos de salarios, los telegramas y/o cartas documento enviados y recibidos si los hay, los estudios médicos y cualquier otra documentación relacionada con el asunto, o que permita identificar al empleador.
+</p>
+              </div>
+              </div>
+
+               <div class="col-guia-descripcion wowload fadeInRight animated" style="visibility: visible;">
+                <img src="img/guia.jpg" style="width:100%;margin-top: 90px;">
+              </div>
+              </div>  
+            </div>
+        </div>
+    </section>
+
+    <!--aside class="bg-dark">
+        <div class="container text-center">
+            <div class="call-to-action">
+                <h2>Free Download at Start Bootstrap!</h2>
+                <a href="#" class="btn btn-default btn-xl wow tada">Download Now!</a>
+            </div>
+        </div>
+    </aside-->
+
+    <section id="contact">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                    <h2 class="section-heading">Let's Get In Touch!</h2>
+                    <hr class="primary">
+                    <p>Ready to start your next project with us? That's great! Give us a call or send us an email and we will get back to you as soon as possible!</p>
+                </div>
+                <div class="col-lg-4 col-lg-offset-2 text-center">
+                    <i class="fa fa-phone fa-3x wow bounceIn"></i>
+                    <p>123-456-6789</p>
+                </div>
+                <div class="col-lg-4 text-center">
+                    <i class="fa fa-envelope-o fa-3x wow bounceIn" data-wow-delay=".1s"></i>
+                    <p><a href="mailto:your-email@your-domain.com">feedback@gabriela.com</a></p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="js/jquery.easing.min.js"></script>
+    <script src="js/jquery.fittext.js"></script>
+    <script src="js/wow.min.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="js/creative.js"></script>
+
+</body>
+
+</html>
