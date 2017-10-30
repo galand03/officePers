aaee9f28bead5cd61b0155118baf59b99b33ad46
galand03 <gal.and85@hotmail.com>
2017-10-29 21:20:47 -0300

    aaa

--- index.php
+++ index.php
@@ -75,6 +75,20 @@
 						<!-- <li><a href="http://freehtml5.co/" class="external">External</a></li> -->
 					</ul>
 				</div>
+				<div class="col-md-12">
+					<!--p class="pull-left">
+						<small class="block">&copy; 2016 Free HTML5. All Rights Reserved.</small> 
+						<small class="block">Designed by <a href="http://freehtml5.co/" target="_blank">FreeHTML5.co</a> Demo Images: <a href="http://unsplash.co/" target="_blank">Unsplash</a></small>
+					</p-->
+					<p class="pull-right">
+						</p><ul class="gtco-social-icons pull-right">
+							<li><a href="#"><i class="icon-twitter"></i></a></li>
+							<li><a href="#"><i class="icon-facebook"></i></a></li>
+							<li><a href="#"><i class="icon-linkedin"></i></a></li>
+							<li><a href="#"><i class="icon-dribbble"></i></a></li>
+						</ul>
+					<p></p>
+				</div>
 			</div>
 			
 		</div>
