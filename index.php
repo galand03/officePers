7f4aaac7ae2cdfe2de7b91abced08083e1bc2dcb
galand03 <galand85@gmail.com>
2019-04-18 21:57:54 -0300

    aa

--- index.php
+++ index.php
@@ -276,7 +276,7 @@
 
 				</div>
 				<div class="col-md-3" style="text-align:center">
-					 <img src="images/andres.png" style="padding-bottom: 30px;    width: 100%;">
+					 <img src="images/andres.1.jpg" style="padding-bottom: 30px;    width: 100%;">
 					 <h3 style="line-height:0">Andrés Galarza</h3>
 					 <h4 style="line-height:0">Director UX. Back-End</h4>
 					 <p style="line-height:0">@andres</p>
