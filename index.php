39fc737093f8d5792049c62b3648df091a1cd7a5
galand03 <galand85@gmail.com>
2019-04-18 23:19:09 -0300

    ll

--- index.php
+++ index.php
@@ -279,19 +279,19 @@
 					 <img src="images/andres.1.jpg" style="padding-bottom: 30px;    width: 100%;">
 					 <h4 style="line-height:0">Andrés Galarza</h4>
 					 <h5 style="line-height:0">Especialista Full Stack</h5>
-					 <p style="line-height:0"><a href="https://www.linkedin.com/in/andres-galarza-9b08a8183/" style="color:blue" target="_blank">@andres<a></p>
+					 <p style="line-height:0"><a href="https://www.linkedin.com/in/andres-galarza-9b08a8183/" style="color:blue" target="_blank">@andres</a></p>
 				</div>
 				<div class="col-md-2" style="text-align:center">
 					<img src="images/fede.jpg" style="padding-bottom: 30px;    width: 100%;">
 				 	<h4 style="line-height:0">Federico Mancuso</h4>
 				 	<h5 style="line-height:0">Especialista Full Stack</h5>
-				 	<p style="line-height:0"><a href="https://www.linkedin.com/in/federico-mancuso-9805ab47/" style="color:blue" target="_blank">@fede<a></p>
+				 	<p style="line-height:0"><a href="https://www.linkedin.com/in/federico-mancuso-9805ab47/" style="color:blue" target="_blank">@fede</a></p>
 				</div>
 				<div class="col-md-2" style="text-align:center">
 					<img src="images/jorge.jpg" style="padding-bottom: 30px;    width: 100%;">
 				 	<h4 style="line-height:0">Jorge Borges</h4>
 				 	<h5 style="line-height:0">Especialista Full Stack</h5>
-				 	<p style="line-height:0"><a href="https://www.linkedin.com/in/jorge-borges-aa461282/" style="color:blue" target="_blank">@jorgito<a></p>
+				 	<p style="line-height:0"><a href="https://www.linkedin.com/in/jorge-borges-aa461282/" style="color:blue" target="_blank">@jorgito</a></p>
 				</div>				
 				<div class="col-md-3" style="text-align:center">
 
@@ -302,13 +302,13 @@
 			
 			<div class="wmuSlider example1">
 			 	<div class="wrap">
-				   <!--article style="position: absolute; width:64%; opacity: 0;"> 
+				   <article style="position: absolute; width:64%; opacity: 0;"> 
 						<div class="cont span_2_of_3">
 						    <h4>Motivación</h4>
 							<p>La creatividad y la simplificación se complementan enormemente. Es preciso hallar nuevas formas alternativas de hacer las cosas. Este pensamiento de diseño exige creatividad.</p>
 						  	<span><a href="#">Bono</a></span>			 
 						</div>
-					</article-->
+					</article>
 					 <article style="position: absolute; width:64%; opacity: 0;"> 
 					   	<div class="cont span_2_of_3">
 							    <h4>Inspiración</h4>
